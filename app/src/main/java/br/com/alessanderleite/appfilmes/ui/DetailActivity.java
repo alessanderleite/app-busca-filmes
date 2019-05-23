f08be3a971670bc6a92913237635218784f1ac60
alessanderleite <alessander.logistica@gmail.com>
2019-05-22 23:55:42 -0300

    create a new empty activity

---
+++ app/src/main/java/br/com/alessanderleite/appfilmes/ui/DetailActivity.java
@@ -0,0 +1,15 @@
+package br.com.alessanderleite.appfilmes.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.alessanderleite.appfilmes.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
