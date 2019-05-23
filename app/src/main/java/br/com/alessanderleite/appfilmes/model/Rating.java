0a280db04815474dcb49b9adb5bf2460f8d92edc
alessanderleite <alessander.logistica@gmail.com>
2019-05-23 11:09:57 -0300

    code the Rating class by using jsonschema2pojo

--- app/src/main/java/br/com/alessanderleite/appfilmes/model/Rating.java
+++ app/src/main/java/br/com/alessanderleite/appfilmes/model/Rating.java
@@ -1,4 +1,32 @@
 package br.com.alessanderleite.appfilmes.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+//using jsonschema2pojo to do the job for you
 public class Rating {
+
+    @SerializedName("Source")
+    @Expose
+    private String source;
+    @SerializedName("Value")
+    @Expose
+    private String value;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
 }
