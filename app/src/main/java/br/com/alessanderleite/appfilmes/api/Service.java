38048fb1fc0aeabc7eb85e0a2f80ed3fca41e5b7
alessanderleite <alessander.logistica@gmail.com>
2019-05-24 08:00:15 -0300

    code the interface class for service

--- app/src/main/java/br/com/alessanderleite/appfilmes/api/Service.java
+++ app/src/main/java/br/com/alessanderleite/appfilmes/api/Service.java
@@ -1,4 +1,12 @@
 package br.com.alessanderleite.appfilmes.api;
 
+import br.com.alessanderleite.appfilmes.model.Movie;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface Service {
+
+    //http://www.omdbapi.com/?i=tt3896198&apikey=a45a2a24
+    @GET("?i=tt3896198&apikey=a45a2a24")
+    Call<Movie> getMovies();
 }
