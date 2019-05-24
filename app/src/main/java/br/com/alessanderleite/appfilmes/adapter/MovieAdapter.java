e12b6e36ad39eaba8a56767b30ca50211abbef3c
alessanderleite <alessander.logistica@gmail.com>
2019-05-24 08:35:25 -0300

    initial code for adapter

---
+++ app/src/main/java/br/com/alessanderleite/appfilmes/adapter/MovieAdapter.java
@@ -0,0 +1,70 @@
+package br.com.alessanderleite.appfilmes.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import br.com.alessanderleite.appfilmes.R;
+import br.com.alessanderleite.appfilmes.model.Movie;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    private List<Movie> movieList;
+    private Context context;
+
+    public MovieAdapter(List<Movie> movieList, Context context) {
+        this.movieList = movieList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_movie, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Movie movie = movieList.get(position);
+
+        Glide.with(context)
+                .load(movie.getPoster())
+                .into(holder.mPoster);
+
+        holder.mTitle.setText(movie.getTitle());
+        holder.mYear.setText(movie.getYear());
+        holder.mRuntime.setText(movie.getRuntime());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView mPoster;
+        private TextView mTitle;
+        private TextView mYear;
+        private TextView mRuntime;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mPoster = (ImageView) itemView.findViewById(R.id.img_poster);
+            mTitle = (TextView) itemView.findViewById(R.id.txt_title);
+            mYear = (TextView) itemView.findViewById(R.id.txt_year);
+            mRuntime = (TextView) itemView.findViewById(R.id.txt_runtime);
+        }
+    }
+}
